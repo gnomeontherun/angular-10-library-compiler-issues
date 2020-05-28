194c2fe0c50fed410812d033ce87db74d70c11ee
Jeremy Wilken <gnomation@gnomeontherun.com>
2020-05-28 09:17:29 -0500

    add library

---
+++ projects/clr-angular/src/lib/clr-angular.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-clr-angular',
+  template: `
+    <p>
+      clr-angular works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ClrAngularComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
